8ea62ab581f77668b5f77f54859bf436c903e56b
Assain2 <mrassain@gmail.com>
2018-08-02 11:44:47 +0200

    add accessibility elements

---
+++ js/tabIndexer.js
@@ -0,0 +1,10 @@
+$("document").ready(function() {
+  var mapFooter = document.getElementsByClassName('leaflet-control-attribution');
+  var mapLinks = mapFooter[0].children;
+  for (var i = 0; i < mapLinks.length; i++){
+    mapLinks[i].tabIndex = -1;
+  }
+
+  var mapElem = document.getElementById('map');
+  mapElem.tabIndex = -1;
+});
